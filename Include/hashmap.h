30343e231c3bd968526ae139ecfeb1a5674517bf
Garret R. <norsemangef@protonmail.com>
2025-08-30 15:45:49 -0600

    added resize function

--- Include/hashmap.h
+++ Include/hashmap.h
@@ -1,8 +1,8 @@
 #include "hash.h"
 #include <cstddef>
 #include <functional>
-#include <stdexcept>
 #include <iostream>
+#include <stdexcept>
 
 #pragma once
 
@@ -10,30 +10,30 @@ const size_t DEFAULT_HASHMAP_BUCKET_COUNT = 16;
 
 template <typename TKey, typename TValue> class Hashmap;
 
-struct key_not_found: public std::logic_error {
-    key_not_found(const char* message):
-        std::logic_error(message)
-    {}
+struct key_not_found : public std::logic_error {
+    key_not_found(const char *message) : std::logic_error(message) {}
 
-    key_not_found():
-        std::logic_error("key not found") 
-    {}
+    key_not_found() : std::logic_error("key not found") {}
 };
 
 template <typename TKey, typename TValue>
-std::ostream& operator<<(std::ostream& out, const Hashmap<TKey, TValue>& map);
+std::ostream &operator<<(std::ostream &out, const Hashmap<TKey, TValue> &map);
 
 template <typename TKey, typename TValue> struct Node {
     TKey key;
     TValue data;
-    Node<TKey, TValue>* next;
-    
-    Node(const TKey& key, const TValue& data);
+    Node<TKey, TValue> *next;
+
+    Node(const TKey &key, const TValue &data);
 };
 
 template <typename TKey, typename TValue> class Hashmap {
     using Node_t = Node<TKey, TValue>;
-public:
+
+  public:
+    #ifdef DEBUG
+        friend void forceResize(Hashmap<TKey, TValue> &map);
+    #endif
     /// @brief default constructor
     Hashmap();
 
@@ -44,43 +44,43 @@ public:
     /// @brief move constructor
     /// @param other map to move from
     Hashmap(Hashmap &&other);
-    
+
     ~Hashmap();
 
     /// @brief attempts to add a new item, fails if the item already exists
     /// @return bool if the operation succeeded
     /// @param key the key of the item to be added
     /// @param value the value of the item to be added
-    bool add(const TKey& key, const TValue& value);
+    bool add(const TKey &key, const TValue &value);
 
     /// @brief adds a new item to the list, overwrites any item of the same key
     /// @param key the key of the item to be added
     /// @param value the value of the item to be added
-    void put(const TKey& key, const TValue& value);
+    void put(const TKey &key, const TValue &value);
 
     /// @brief removes the item at the key
     /// @throws key_not_found if the key was not found
     /// @return TValue the value of the item that was removed
     /// @param key the key of the item to be removed
-    TValue remove(const TKey& key);
+    TValue remove(const TKey &key);
 
     /// @brief checks if there is an item with that key
     /// @returns bool if the key exists, return true; else false
     /// @param key the key of the item to check
-    bool contains(const TKey& key) const;
+    bool contains(const TKey &key) const;
 
     /// @brief gets the value attatched to the key
     /// @returns TValue& the value which was attatched to the key
     /// @param key the key of the item we want to get
     /// @throws key_not_found if the key was not found
-    TValue& get(const TKey& key);
+    TValue &get(const TKey &key);
 
     /// @brief gets the value attatched to the key
     /// @returns const TValue& the value which was attatched to the key
     /// @param key the key of the item we want to get
     /// @throws key_not_found if the key was not found
-    const TValue& get(const TKey& key) const;
-    
+    const TValue &get(const TKey &key) const;
+
     /// @brief returns the number of items in the map
     /// @returns size_t the number of items in the map
     size_t size() const;
@@ -88,39 +88,48 @@ public:
     /// @brief removes all data in the map
     void clear();
 
-    Hashmap<TKey, TValue>& operator=(const Hashmap<TKey, TValue>& map); // copy operator
+    Hashmap<TKey, TValue> &
+    operator=(const Hashmap<TKey, TValue> &map); // copy operator
 
-    Hashmap<TKey, TValue>& operator=(Hashmap<TKey, TValue>&& map); // move operator
-    
+    Hashmap<TKey, TValue> &
+    operator=(Hashmap<TKey, TValue> &&map); // move operator
+
     /// @brief makes a new map, formed by combining two others
     /// @returns Hashmap<TKey, TValue> the new map to be created
     /// @param other the map to add to the current map
-    /// @remarks if the two maps contain items with the same keys, the conflicting items of the right map will be ignored.
-    Hashmap<TKey, TValue> operator+(const Hashmap<TKey, TValue>& other) const;
+    /// @remarks if the two maps contain items with the same keys, the
+    /// conflicting items of the right map will be ignored.
+    Hashmap<TKey, TValue> operator+(const Hashmap<TKey, TValue> &other) const;
 
     /// @brief modifies this map by adding the items of another map
     /// @returns Hashmap<TKey, TValue>& a reference to this map
     /// @param other the map to add to the current map
-    /// @remarks if the two maps contain items with the same keys, the conflicting items of the right map will be ignored.
-    Hashmap<TKey, TValue>& operator+=(const Hashmap<TKey, TValue>& other);
+    /// @remarks if the two maps contain items with the same keys, the
+    /// conflicting items of the right map will be ignored.
+    Hashmap<TKey, TValue> &operator+=(const Hashmap<TKey, TValue> &other);
 
-    bool operator==(const Hashmap<TKey, TValue>& other) const;
+    bool operator==(const Hashmap<TKey, TValue> &other) const;
 
-    bool operator!=(const Hashmap<TKey, TValue>& other) const;
+    bool operator!=(const Hashmap<TKey, TValue> &other) const;
 
-    friend std::ostream& operator<< <>(std::ostream& out, const Hashmap<TKey, TValue>& map);
+    friend std::ostream &operator<< <>(std::ostream &out,
+                                       const Hashmap<TKey, TValue> &map);
 
-private:
-    Node_t** _buckets;
+  private:
+    Node_t **_buckets;
     size_t _bucket_count;
     size_t _item_count;
 
     Hashmap(int count);
 
-    void copy_from(Node_t** source, size_t size);
-    Node_t* get_node(hash_t hval, const TKey& key);
-    const Node_t* get_node(hash_t hval, const TKey& key) const;
-    void add_node(hash_t hval, const TKey& key, const TValue& value);
+    void copy_from(Node_t **source, size_t size);
+    Node_t *get_node(hash_t hval, const TKey &key);
+    const Node_t *get_node(hash_t hval, const TKey &key) const;
+    void add_node(hash_t hval, const TKey &key, const TValue &value);
+    void add_node(hash_t hval, const TKey &key, const TValue &value,
+                  Node_t **target_buckets, size_t target_count);
+
+    void resize();
 };
 
 #include "hashmap.inc"
